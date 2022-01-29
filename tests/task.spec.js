b1be366f3dd660791bddd4fb7ba9ac591109a641
ricomasgu <ricard.comas.gomez@gmail.com>
2022-01-29 13:51:51 +0100

    Starting to define the tests for the task class and creating files for other classes

--- tests/task.spec.js
+++ tests/task.spec.js
@@ -12,9 +12,51 @@ describe('Task', () => {
     });
 
     describe('constructor method', () => {
-        it('should receive `value` as a parameter and store the value in its own `value` property', () => {
+        it('should receive AT LEAST `name` as a parameter and store the value in its own `name` property', () => {
             expect(Task.constructor.length).toBe(1);
-            expect(task.value === value).toBe(true);
+            expect(task.name).toBe(true);
+            expect(task.description).toBe(true);
+            expect(task.time).toBe(true);
+            expect(task.startDate).toBe(true);
+            expect(task.endDate).toBe(true);
+            expect(task.startPlannedDate).toBe(true);
+            expect(task.endPlannedDate).toBe(true);
+            expect(task.status).toBe(true);
+            expect(task.assignedResources).toBe(true);
+            expect(task.priority).toBe(true);
+            expect(task.notes).toBe(true);
+            expect(task.chat).toBe(true);
+            expect(task.files).toBe(true);
+            expect(task.issues).toBe(true);
+            expect(task.meetings).toBe(true);
+        });
+    });
+
+    describe('createSubTask method', () => {
+        beforeEach(() => {
+          task = new Task();
+        });
+    
+        it('should be declared', () => {
+          expect(typeof task.createSubTask).toBe('function');
+        });
+    
+        it('should return the value of the task', () => {
+            expect(task.createSubTask() === task.value).toBe(true);
+        });
+    });
+
+    describe('deleteSubTask method', () => {
+        beforeEach(() => {
+          task = new Task();
+        });
+    
+        it('should be declared', () => {
+          expect(typeof task.deleteSubTask).toBe('function');
+        });
+    
+        it('should return the value of the task', () => {
+            expect(task.deleteSubTask() === task.value).toBe(true);
         });
     });
 
